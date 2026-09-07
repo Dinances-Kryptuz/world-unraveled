7c4a1b83eff63eeb3fe9d896e9828e0f07c9a894
Dinances-Kryptuz <jordenfinch1@gmail.com>
2026-09-07 19:51:13 -0400

    Create types.ts

---
+++ src/gameData/talents/types.ts
@@ -0,0 +1,47 @@
+import type { SpecId } from '../classStats';
+
+export type TalentEffectKind =
+  | 'flat_dmg'
+  | 'flat_dmg_taken'
+  | 'every_n'
+  | 'chance_mult'
+  | 'ramp'
+  | 'above_hp'
+  | 'below_hp'
+  | 'self_below_hp_dmg'
+  | 'self_below_hp_mitigation'
+  | 'armor_mult'
+  | 'hp_mult'
+  | 'surv_coef_mult'
+  | 'avoidance_add'
+  | 'heal_frac_add'
+  | 'heal_mult'
+  | 'passive_heal_add'
+  | 'once_per_fight'
+  | 'dead';
+
+export interface TalentEffect {
+  kind: TalentEffectKind;
+  params: number[];
+  note?: string;
+}
+
+export type TalentColumn = 'damage' | 'survival' | 'support';
+
+export interface TalentOption {
+  name: string;
+  description: string;
+  effect: TalentEffect;
+}
+
+export interface TalentRow {
+  level: number;
+  damage: TalentOption;
+  survival: TalentOption;
+  support: TalentOption;
+}
+
+export type TalentTree = TalentRow[];
+
+export type TalentPicks = Partial<Record<number, TalentColumn>>;
+</br>
